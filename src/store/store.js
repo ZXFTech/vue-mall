17ab7a5a423bafcf13e85cc8a7e0928074310daa
feline <zxf_tech@outlook.com>
2020-06-14 16:29:58 +0800

    购物车和vuexstore配置

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,17 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+import mutations from './mutations'
+import actions from './actions'
+import getters from './getters'
+
 const store = new Vuex.Store({
+  state :{
+    cartGoods:[]
+  },
+  mutations,
+  actions,
+  getters
 })
 
 export default store
